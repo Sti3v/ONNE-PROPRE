b0440f67c6719a15aa61d05aa0b471590f1eb2ca
Sti3v <sti3v355@gmail.com>
2023-03-16 20:08:36 +0100

    tkt

--- js/175pourcent.js
+++ js/175pourcent.js
@@ -1,5 +1,11 @@
+//** Ce bout de code permet de limiter la taille de la a 175% */
+
+setInterval(function () {
+  checkZoomLevel();
+}, 2500);
+
 function checkZoomLevel() {
-  var zoomLevel = Math.round(window.devicePixelRatio * 100);
+  let zoomLevel = Math.round(window.devicePixelRatio * 100);
   if (zoomLevel > 175) {
     alert("Veuillez baisser le niveau de zoom avant de continuer ! Vous ne pouvez pas depasser 175%!");
     setTimeout(function () {
@@ -8,6 +14,4 @@ function checkZoomLevel() {
   }
 }
 
-setInterval(function () {
-  checkZoomLevel();
-}, 2500); // Vérification toutes les 1000ms (1 seconde)
+ 
