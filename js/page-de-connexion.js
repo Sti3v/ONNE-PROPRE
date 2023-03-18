01e3758f309fd50b56d0d768bb7628a8a6c8da74
Sti3v <sti3v355@gmail.com>
2023-03-18 15:01:18 +0100

    eye open and close for password

--- js/page-de-connexion.js
+++ js/page-de-connexion.js
@@ -53,3 +53,20 @@ inputs.forEach((input, index) => {
   });
 });
 
+
+
+const passwordInput = document.getElementById("password");
+const togglePassword = document.getElementById("toggle-password");
+
+togglePassword.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  } else {
+    passwordInput.type = "password";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  }
+});
+
